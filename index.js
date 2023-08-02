9aeb193682bd6f73c27f33b2196b71dc5de7ef13
Alan Chelko <achelko@gmail.com>
2023-08-02 21:15:47 +0000

    Minor edit.

--- index.js
+++ index.js
@@ -13,5 +13,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is happily running on port ${port}!`);
 });
